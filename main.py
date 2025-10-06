cd6751747e8407d827f7cdeddd1a301ae8db0eea
JD0ud <jonathan_doud@taylor.edu>
2025-10-06 16:36:08 -0400

    Completed CSS styling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-# from fastapi.templating import Jinja2Templates
 from .core.templates import templates
 from fastapi import Request, Form, WebSocket, WebSocketDisconnect, Cookie
 from fastapi.responses import HTMLResponse, RedirectResponse
@@ -24,32 +23,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(cards.router)
 app.include_router(sets.router)
 
-# templates = Jinja2Templates(directory = "templates")
-
-# class Set(SQLModel, table = True):
-#     id: int | None = Field(default = None, primary_key = True)
-#     name: str
-#     cards: list["Card"] = Relationship(back_populates = "set")
-    # user_id: int
-
-# class Card(SQLModel, table = True):
-#     id:int | None = Field(default = None, primary_key = True)
-#     question:str
-#     answer:str
-#     set_id: int | None = Field(default = None, foreign_key = "set.id")
-#     set: Set | None = Relationship(back_populates = "cards")
-#     incorrect_guesses: int
-
-# class User(BaseModel):
-#     id: int
-#     name: str
-#     email: str
-#     set_id: int
-
-class Deck(BaseModel):
-    id: int
-    name: int
-
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
@@ -67,24 +40,11 @@ class ConnectionManager:
         
 manager = ConnectionManager()
 
-# setList = [
-#     Set(id=1, name="Geography"),
-#     Set(id=2, name="Animals"),
-#     Set(id=3, name="Linguistics")
-# ]
-
-# cardList = [Card(id = 1, question = "Where is Taylor University located?", answer = "Upland, IN, USA", set_id = 1, incorrect_guesses = 0),
-#             Card(id = 2, question = "What is the airspeed velocity of an unladen swallow?", answer = "~24 mph (European)", set_id = 2, incorrect_guesses = 0),
-#             Card(id = 3, question = "What is a group of cats called?", answer = "Clowder", set_id = 2, incorrect_guesses = 0),
-#             Card(id = 4, question = "What is the longest commonly accepted word in the English language?", answer = "Pneumonoultramicroscopicsilicovolcanoconiosis", set_id = 3, incorrect_guesses = 0),
-#             Card(id = 5, question = "In which U.S. state is it illegal to eat fried chicken with a fork?", answer = "Georgia", set_id = 1, incorrect_guesses = 0)]
-
 userList = [User(id = 1, name = "Monty123", email = "caerbannog@gmail.com", set_id = 2),
             User(id = 2, name = "helpicantthinkofaname", email = "helpicantthinkofanemail@yahoo.com", set_id = 1)]
 
 @app.get("/", response_class = HTMLResponse)
 async def root(session: SessionDep, request:Request):
-    # return templates.TemplateResponse(request = request, name = "index.html", context = {"cards": cardList})
     cards = session.exec(select(Card).order_by(Card.id)).all()
     sets = session.exec(select(Set).order_by(Set.id)).all()
     return templates.TemplateResponse(request=request, name="/index.html", context={"cards":cards,"sets":sets})
@@ -98,16 +58,11 @@ async def getCards(q:str=""):
 
 @app.get("/cards/{card_id}", response_class = HTMLResponse)
 async def getCardByID(card_id:int, request:Request, session:SessionDep):
-    # for card in cardList:
-        # if card.id == card_id: return templates.TemplateResponse(request = request, name = "card.html", context = {"card": cardList[card_id - 1]})
-    card = session.exec(select(Card).where(Card.id == card_id))
+    card = session.exec(select(Card).where(Card.id == card_id)).first()
     return templates.TemplateResponse(request = request, name = "card.html", context = {"card":card})
 
 @app.get("/sets/{set_id}", response_class = HTMLResponse)
 async def getSetByID(session: SessionDep, set_id:int, request:Request):
-    # for set in setList:
-    #     if set.id == set_id: return templates.TemplateResponse(request = request, name = "set.html", context = {"set": cardList[card_id - 1]})
-    # return None
     set = session.exec(select(Set).where(Set.id == set_id)).first()
     cards = set.cards
     return templates.TemplateResponse(request=request, name="/set.html", context={"set":set, "cards":cards})
@@ -117,12 +72,13 @@ async def getAddSet(request:Request, session:SessionDep, response_class = HTMLRe
     return templates.TemplateResponse(request=request, name="/sets/add.html")
 
 @app.post("/sets/add")
-async def addSet(session: SessionDep, name:str = Form(...)):
+async def addSet(request:Request, session: SessionDep, response_class = HTMLResponse, name:str = Form(...)):
     db_set = Set(name=name)
     session.add(db_set)
     session.commit()
     session.refresh(db_set)
-    return db_set
+    sets = session.exec(select(Set).order_by(Set.name)).all()
+    return templates.TemplateResponse(request=request, name="/sets/sets.html", context={"sets":sets})
 
 @app.get("/card/add")
 async def getAddCard(request:Request, session:SessionDep, response_class = HTMLResponse):
